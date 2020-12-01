46493ccf563dd44f8d231af52dd996de51dbb745
Alex B <alexb@MacBook-Air-Alex.local>
2020-12-01 17:06:51 +0100

    Tabla 3 en raya

---
+++ Tres en Raya.py
@@ -0,0 +1,19 @@
+# Tres en raya!
+
+import random
+
+def dibujaTabla(tabla):
+    # This function prints out the board that it was passed.
+
+    # "Tabla" es una lista de 10 strings que representan la tabla.
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
