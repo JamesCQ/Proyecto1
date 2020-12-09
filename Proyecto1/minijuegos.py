156a246f9bfe53bfc87d3d96881bccd3887f872e
cgranda3 <cgranda@cifpfbmoll.eu>
2020-12-09 19:08:34 +0100

    correccion

--- Proyecto1/minijuegos.py
+++
@@ -1,6 +0,0 @@
-import juego1
-import juego2
-import juego3
-
-print("=========================== \nMinijuegos \n===========================")
-print("1)   Domino \n2)   Ajedrez \n3)   Parchís \n===========================")
