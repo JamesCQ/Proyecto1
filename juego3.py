072ccbbca0d522826b5b3aa32d5ac70b2b1c6d35
JamesCQ <jamescaq@gmail.com>
2020-12-13 19:35:02 +0100

    commit domingo james

--- juego3.py
+++
@@ -1,5 +0,0 @@
-
-puntosdadosrojo = []
-puntosdadoamarillo = []
-puntosdadoazul = []
-puntosdadoverde = []
--- minijuegos.py
+++ minijuegos.py
@@ -1,20 +1,25 @@
-import juego1
-import juego3
-import Tres_en_raya
+from parchis import menu_parchis
 
-menu = ("=========================\n=      M E N Ú          =\n=========================\n 1) Juego 1\n 2) Juego 2\n 3) Juego 3\n 4) salir\n=========================")
-print(menu)          #Imprimo el menu que he hecho a mano con string
-opcion=int(input("¿Qué juego deseas jugar? "))       #pregunto la opcion con un int
-
-while (opcion!=4):         #mientras que cuando te pida la opcion y no sea un 4 (salir), no se termina el bucle while
-    if (opcion==1):
-
-        print(menu)
+def menu():
+    salir=False
+    while salir==False:
+        print("=========================")
+        print("=    MENÚ MINIJUEGOS    =")
+        print("=========================")
+        print(" 1) La oca")
+        print(" 2) Tres en raya")
+        print(" 3) Parchís")
+        print(" 4) Salir")
         opcion=int(input("¿Qué juego deseas jugar? "))
-    elif (opcion==2):
+        if (opcion==1):
+            print("1")
+        elif (opcion==2):
+            print("2")
+        elif (opcion==3):
+            menu_parchis()
+        elif (opcion==4):
+            salir=True
+        else:
+            print("Introduce de nuevo, la opcion fue incorrecta: ")
 
-        print(menu)
-        opcion=int(input("¿Qué juego deseas jugar? "))
-    elif (opcion==3):
-        print(menu)
-        opcion=int(input("¿Qué juego deseas jugar? "))
+menu()
